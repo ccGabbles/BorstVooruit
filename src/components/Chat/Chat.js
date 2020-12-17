b7e7f377c0913fe5dc59914176d0732f4da549f8
gabriel.moawad <gabriel.moawad@capgemini.com>
2020-12-17 14:27:16 +0100

    Chat component placeholder & first map

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Chat.css';
+
+const Chat = () => (
+  <div className="Chat">
+    Chat Component
+  </div>
+);
+
+export default Chat;
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './map.scss';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import './Map.css';
 
-const map = () => (
-  <div className="map">
-    map Component
-  </div>
-);
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZ21vYXdhZCIsImEiOiJja2lzcjRmamQwbmJoMnBzY2oycnpldzRuIn0.OWucPatEQEoNmhwRmqFXqg';
 
-map.propTypes = {};
+const Map = () => {
+  const mapContainerRef = useRef(null);
 
-map.defaultProps = {};
+  const [lng, setLng] = useState(5);
+  const [lat, setLat] = useState(34);
+  const [zoom, setZoom] = useState(1.5);
 
-export default map;
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      <div className='sidebarStyle'>
+        <div>
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+      </div>
+      <div className='map-container' ref={mapContainerRef} />
+    </div>
+  );
+};
+
+export default Map;
