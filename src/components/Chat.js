77126bddec0ae7e6a3edf110122b03861ed2f2c1
StephBerg86 <info@stephaniebergman.nl>
2020-12-17 15:03:41 +0100

    displayed message

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 
 export default function Chat() {
-  const [message, setMessage] = useState()
+  const [newMessage, setNewMessage] = useState("")
+const [messages, setMessages] = useState([])
   
   function submitForm(event) {
-    setMessage("")
+    event.preventDefault()
+
+    setMessages([...messages, newMessage])
+
+    setNewMessage("")
   }
    
   return (
@@ -19,13 +23,21 @@ export default function Chat() {
     <Card.Title>Borst Vooruit Chat</Card.Title>
     <InputGroup className="mb-3">
     <FormControl
-      placeholder="Type tekst"
-      value={message}
+      placeholder="Type your message..."
+      type="text"
+      onChange={(event) => setNewMessage(event.target.value)}
+      value={newMessage}
     />
   
   </InputGroup>
-    <Button variant="primary" type="submit" onClick={submitForm}>Verzend</Button>
+    <button className="chatButton" type="submit" onClick={submitForm}>Verzend</button>
   </Card.Body>
+{messages.map((message)=> {
+  return ( <div style={{border: "solid 1px grey", margin: "0.5rem"}} >
+
+  <p>{message}</p>
+  </div>)
+})}
 </Card>
 </div>
   )
