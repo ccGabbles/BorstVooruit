c4933a7d9578d2a1069b795b48897eaf88ac9014
Caroline Kilg <caroline@kilg.nl>
2020-12-17 16:54:55 +0100

    intro page

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Button } from '../Button.js';
+import jeroen from  '../../JeroenHomescreen.png';
+
 
 const IntroPage = () => (
   <div className="IntroPage">
-    Intro Page
-  </div>
+           <h1>Borst Vooruit</h1>
+           <img src={jeroen} className="jeroen-home" alt="Jeroen homescreen" />
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  </form>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+        >
+          Accept Challenge
+        </Button>
+        </a>
+    </div>
 );
 
 export default ActivityPage;
