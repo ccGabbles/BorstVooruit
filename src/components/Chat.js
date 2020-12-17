6904168e4e96b8a6ebbd720e30aaa600df183500
Sander Schekman <sanderschekman@gmail.com>
2020-12-17 15:08:59 +0100

    feat: basic implementation of socket.io-client

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import socketIOClient from 'socket.io-client'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
+import { ENDPOINT } from '../constants'
 
 export default function Chat() {
   const [message, setMessage] = useState()
+  const [response, setResponse] = useState()
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT)
+    
+    socket.on("FromAPI", data => {
+      setResponse(data)
+    });
+  }, [])
   
   function submitForm(event) {
     setMessage("")
